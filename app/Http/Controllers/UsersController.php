a65316df27c70293152baa5a92fc06bf161e8b95
lijian <852688838@qq.com>
2019-12-24 16:24:50 +0800

    用户注册表单

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,13 +8,21 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    //注册会员页面
     public function create()
     {
         return view('users.create');
     }
 
+    //某个用户页面
     public function show(User $user)
     {
         return view('users.show',compact('user'));
+    }
+
+    //添加注册会员
+    public function store()
+    {
+
     }
 }
