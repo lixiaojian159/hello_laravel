0ce2a79d8036f8c73db27b3e76635bf3c8b84703
lijian <852688838@qq.com>
2019-12-30 17:13:02 +0800

    首页微博列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,10 @@ class User extends Authenticatable
     {
         return $this->hasMany(Status::class);
     }
+
+    //获取当前用户的微博
+    public function feed()
+    {
+        return $this->statuses()->orderBy('created_at','desc');
+    }
 }
